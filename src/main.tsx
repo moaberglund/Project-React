2be370faba4c12f0aa3fb4b7faa755bce062e45a
moaberglund <moa.21@hotmail.com>
2025-03-07 11:30:50 +0100

    import main SCSS file for styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// SCSS main import
+import './styles/main.scss'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
