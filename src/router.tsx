38af61438eb8107a9ef39fa6b20293fde27f17c8
moaberglund <moa.21@hotmail.com>
2025-03-07 17:37:00 +0100

    add route for BookPage to display book details

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import BookPage from "./pages/book/[id]";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <HomePage />
+    },
+    {
+        path: "/book/:id",
+        element: <BookPage />
     }
 ])
