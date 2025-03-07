a27e9fac855e1912b67f50a7d13d2a03e6daf2bd
moaberglund <moa.21@hotmail.com>
2025-03-07 14:17:18 +0100

    refactor BookList to use BookSmall component for rendering fiction books

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,19 +1,28 @@
 import booksData from './../../books.json';
+import BookSmall from './BookSmall';
 
 const BookList = () => {
   // Filtrera böcker som har "Fiction" som kategori
-  const fictionBooks = booksData.filter(book => 
+  const fictionBooks = booksData.filter(book =>
     book.volumeInfo.categories && book.volumeInfo.categories.includes("Fiction")
   );
 
   return (
     <div>
       <h1>Fiction Books</h1>
-      <ul>
+
+      <div className='grid'>
+
         {fictionBooks.map((book, index) => (
-          <li key={index}>{book.volumeInfo.title}</li>
+          <BookSmall thumbnail={book.volumeInfo.imageLinks?.smallThumbnail}
+            title={book.volumeInfo.title}
+            authors={book.volumeInfo.authors}
+            publishedDate={book.volumeInfo.publishedDate}
+            pageCount={book.volumeInfo.pageCount}
+            key={index} />
+
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
