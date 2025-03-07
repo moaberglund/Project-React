5a865bcea4e8889493945ecb7068d8cb1b07bc18
moaberglund <moa.21@hotmail.com>
2025-03-07 17:36:50 +0100

    add BookLarge component for displaying detailed book information

---
+++ src/components/BookLarge.tsx
@@ -0,0 +1,25 @@
+import BookLargeProps from "../interfaces/BookLargeProps"
+
+const BookLarge = ({ id, thumbnail, title, authors, publishedDate, pageCount, description, publisher, categories }: BookLargeProps) => {
+    return (
+        <>
+            <div id={id} className="flex book-large">
+                <div className="flex-1">
+                    <img src={thumbnail} alt="Book cover" />
+                </div>
+
+                <div className="flex-3">
+                    <h1>{title}</h1>
+                    <h2>{authors}</h2>
+                    <p>{pageCount} pages</p>
+                    <p>Published {publishedDate} by {publisher}</p>
+                    <p>{description}</p>
+                    <p>Categories: {categories.join(", ")}</p>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default BookLarge
