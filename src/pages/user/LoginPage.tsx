05497c5d422bc1b69482a7b2708ae5dfff16d3b3
moaberglund <moa.21@hotmail.com>
2025-03-10 12:53:17 +0100

    add LoginPage component for user authentication

---
+++ src/pages/user/LoginPage.tsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+const LoginPage = () => {
+
+  // States
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  const { login, user } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(user) {
+      navigate(`/user/${user._id}`);
+    }
+}, [user]);
+
+
+  // Functions
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError('');
+    setIsLoading(true);
+
+
+    try {
+      await login({ username, password });
+      
+      navigate(`/user/${user?._id}`);
+
+
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Failed to login. Control username and password");
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+
+  return (
+    <div>
+      <h1 className="login">Welcome Back</h1>
+
+      <div className="auth-form">
+
+        <form onSubmit={handleSubmit}>
+          {error && <p className="error">{error}</p>}
+
+
+          <input
+            aria-label="username"
+            type="text"
+            id="username"
+            name="username"
+            required
+            value={username}
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+            disabled={isLoading} />
+
+          <input
+            aria-label="password"
+            type="password"
+            id="password"
+            name="password"
+            required
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            disabled={isLoading} />
+
+          <button
+            className="btn login-btn"
+            type="submit"
+            disabled={isLoading}>
+            {isLoading ? 'Logging in...' : 'Login'}
+          </button>
+
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
+
