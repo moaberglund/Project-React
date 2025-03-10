c7f0bafe66016c46da5b11745dd013e5fc60a857
moaberglund <moa.21@hotmail.com>
2025-03-10 13:22:57 +0100

    add SignUpPage component for user registration functionality

---
+++ src/pages/user/SignUpPage.tsx
@@ -0,0 +1,78 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import { useState } from "react";
+
+const SignUpPage = () => {
+
+  // States
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { register } = useAuth();
+  const navigate = useNavigate();
+
+  // Functions
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError('');
+
+    try {
+      await register({ username, password });
+      navigate('/user/login');
+    } catch (err) {
+      setError("Failed to register. Control username and password");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div>
+
+      <h1 className="signup">Create Account</h1>
+
+      <div className="auth-form">
+
+        <form onSubmit={handleSubmit}>
+          {error && <p className="error">{error}</p>}
+
+          <input
+            aria-label="username"
+            type="username"
+            id="username"
+            name="username"
+            required value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+            disabled={isLoading} />
+
+          <input
+            aria-label="password"
+            type="password"
+            id="password"
+            name="password"
+            required value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            disabled={isLoading} />
+
+          <button
+            className="btn signin-btn"
+            type="submit"
+            disabled={isLoading}>
+            {isLoading ? 'Signing up...' : 'Sign up'}
+          </button>
+
+        </form>
+
+        <p>Already a user?  <NavLink to="/user/login">Log in here</NavLink></p>
+      </div>
+
+    </div>
+  )
+}
+
+export default SignUpPage
