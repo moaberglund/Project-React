1db09abd26776a95e7e0726c848376ac39d2e0e9
moaberglund <moa.21@hotmail.com>
2025-03-10 10:08:06 +0100

    update router to include MainLayout and add UserPage route

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import BookPage from "./pages/book/[id]";
+import UserPage from "./pages/user/[id]";
+import MainLayout from "./layouts/MainLayout";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <HomePage />
-    },
-    {
-        path: "/book/:id",
-        element: <BookPage />
+        element: <MainLayout />,
+        children: [
+            {
+                path: "/",
+                element: <HomePage />
+            },
+            {
+                path: "/book/:id",
+                element: <BookPage />
+            },
+            {
+                path: "/user/:id",
+                element: <UserPage />
+            }
+
+        ]
     }
+
 ])
