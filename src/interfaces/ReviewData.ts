d5ddc7a6ea4d57466c8908490db2218f66c78a7e
moaberglund <moa.21@hotmail.com>
2025-03-10 13:53:16 +0100

    add ReviewData interface to define structure for review data management

---
+++ src/interfaces/ReviewData.ts
@@ -0,0 +1,13 @@
+interface ReviewData {
+    _id: string;
+    rating: number;
+    title: string;
+    text: string;
+    user: string | object;
+    book_id: string;
+    likes?: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export default ReviewData;
