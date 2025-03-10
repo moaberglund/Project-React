fcd1a91a459cf52582be4df99ed1dd999ddae211
moaberglund <moa.21@hotmail.com>
2025-03-10 12:52:24 +0100

    update user link in AppHeader to navigate to the authenticated user profile

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,8 +1,10 @@
 import { NavLink } from "react-router-dom"
 import { FaHouse, FaRegUser } from "react-icons/fa6";
+import { useAuth } from "../context/AuthContext";
 
 
 const AppHeader = () => {
+    const { user } = useAuth();
     return (
         <>
             <header>
@@ -11,7 +13,8 @@ const AppHeader = () => {
                 <nav>
                     <ul>
                         <li><NavLink to={"/"}><FaHouse /> Home</NavLink></li>
-                        <li><NavLink to={"/user/:id"}><FaRegUser /></NavLink></li>
+
+                        <li><NavLink to={`/user/${user?._id}`}><FaRegUser /></NavLink></li>
                     </ul>
                 </nav>
             </header>
