b68eb96bf1c52163769861fe748b3ede3079b455
moaberglund <moa.21@hotmail.com>
2025-03-11 15:53:02 +0100

    add: create Quote component with a list of recommended books

---
+++ src/components/Quote.tsx
@@ -0,0 +1,17 @@
+
+const Quote = () => {
+    return (
+        <div className="quote">
+            <h2>List of books to read before you die:</h2>
+            <ol>
+                <li>Any book you want.</li>
+                <li>Don't read books you don't want to read.</li>
+                <li>That's it.</li>
+                <li>Congratulations, you did it!</li>
+            </ol>
+
+        </div>
+    )
+}
+
+export default Quote
