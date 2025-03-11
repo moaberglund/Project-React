2a930770da738ef74aa004d12bac9c4de0a72bfe
moaberglund <moa.21@hotmail.com>
2025-03-11 11:45:09 +0100

    add: fetch and display book reviews with loading state handling

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -3,14 +3,41 @@ import BookLarge from "../../components/BookLarge"
 import { NavLink, useNavigate, useParams } from 'react-router-dom';
 import CreateBookReview from '../../components/CreateBookReview';
 import BookShelfButton from '../../components/BookshelfButton';
+import BookReview from '../../components/BookReview';
+import { useEffect, useState } from 'react';
+import ReviewData from '../../interfaces/ReviewData';
 
 
 const BookPage = () => {
 
     const user = localStorage.getItem("user");
-    const { id } = useParams<{ id: string }>();  // Hämtar id från URL
-    const book = booksData.find(book => book.id === id);  // Hitta boken från JSON eller API
+    const { id } = useParams<{ id: string }>();  // Get id from URL
+    const book = booksData.find(book => book.id === id);  // Get book from JSON (HAVE TO UPDATE TO API in future)
     const navigate = useNavigate();
+
+    // States
+    const [reviews, setReviews] = useState<ReviewData[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    // Fetch reviews of book
+    useEffect(() => {
+        const fetchReviews = async () => {
+            try {
+                const response = await fetch(`https://read-y-api.onrender.com/review/book/${id}`);
+                if (response.ok) {
+                    const data = await response.json();
+                    setReviews(data);  // Update state with reviews
+                }
+            } catch (error) {
+                console.error("Error fetching reviews:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchReviews();
+    }, [id]);  // Runs when id changes
+
 
     if (!book) {
         return <p>Book not found</p>;
@@ -30,15 +57,22 @@ const BookPage = () => {
                 categories={book.volumeInfo.categories}
             />
 
-            {user ? <BookShelfButton bookId={book.id} /> : <button><NavLink to={"/user/login"}>Want to Read</NavLink></button> }
+            {user ? <BookShelfButton bookId={book.id} /> : <button><NavLink to={"/user/login"}>Want to Read</NavLink></button>}
 
             {user ? <CreateBookReview /> : <NavLink to="/login">Login to create a review</NavLink>}
 
+            <h2>Reviews</h2>
+            {loading ? <p>Loading reviews...</p> : (
+                reviews.length > 0 ? (
+                    reviews.map((review) => (
+                        <BookReview key={review._id} {...review} />
+                    ))
+                ) : (
+                    <p>No reviews yet.</p>
+                )
+            )}
 
             <button onClick={() => navigate(-1)}>Back</button>
-
-
-
 
         </>
     )
