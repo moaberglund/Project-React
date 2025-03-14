da87ff6b8fe48f090bb516f37795a2a85ec072bd
moaberglund <moa.21@hotmail.com>
2025-03-14 13:49:47 +0100

    Revert "style: enhance SignUpPage with Yup validation and improved error handling"
    This reverts commit aa029019e5134da30f6dfb487a59de245ec47066.

--- src/pages/user/SignUpPage.tsx
+++ src/pages/user/SignUpPage.tsx
@@ -1,62 +1,29 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import { useState } from "react";
-import * as Yup from "yup";
-
-// For ErrorMessages
-interface ErrorMessage {
-  message: string;
-  type: 'username' | 'password' | 'general';
-}
 
 const SignUpPage = () => {
 
   // States
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState<ErrorMessage | null>(null);
+  const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const { register } = useAuth();
   const navigate = useNavigate();
 
-
-  // Yup 
-  const validationSchema = Yup.object({
-    username: Yup.string().required("Please enter a username.")
-      .min(5, "Username must be at least 5 characters long.")
-      .max(50, "Username can't be longer than 50 characters."),
-    password: Yup.string().required("Please enter a password.")
-      .min(8, "Password must be at least 6 characters long.")
-      .max(20, "Password can't be longer than 20 characters."),
-  });
-
-
   // Functions
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsLoading(true);
-    setError(null);
+    setError('');
 
     try {
-      // Validate the username and password
-      await validationSchema.validate({ username, password }, { abortEarly: false });
-
       await register({ username, password });
       navigate('/user/login');
     } catch (err) {
-      if (err instanceof Yup.ValidationError) {
-        // Create an object with the errors
-        setError({
-          message: err.errors.join(", "),
-          type: err.path === 'username' ? 'username' : 'password',
-        });
-      } else {
-        setError({
-          message: "Failed to register. Control username and password.",
-          type: 'general',
-        });
-      }
+      setError("Failed to register. Control username and password");
     } finally {
       setIsLoading(false);
     }
@@ -80,7 +47,6 @@ const SignUpPage = () => {
             onChange={(e) => setUsername(e.target.value)}
             placeholder="Username"
             disabled={isLoading} />
-          {error && error.type === "username" && <p style={{ color: "red" }}>{error.message}</p>}
 
           <input
             aria-label="password"
@@ -91,7 +57,6 @@ const SignUpPage = () => {
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
             disabled={isLoading} />
-          {error && error.type === "password" && <p style={{ color: "red" }}>{error.message}</p>}
 
           <button
             style={{ marginTop: "1em" }}
@@ -100,6 +65,8 @@ const SignUpPage = () => {
             disabled={isLoading}>
             {isLoading ? 'Signing up...' : 'Sign up'}
           </button>
+
+          {error && <p style={{ color: "red", fontSize: "0.9em", margin: "1em 0 2em 0"}}>{error}</p>}
 
         </form>
 
